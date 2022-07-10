55cb218a1253413de1f51476c2a9e350f945c5c4
Lucas Cavalcante <84925398+LUCASDEV001@users.noreply.github.com>
2022-07-09 21:30:10 -0300

    commit interhost

---
+++ script.js
@@ -0,0 +1,11 @@
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal("h1", { duration: 1000 });
+sr.reveal("h2", { duration: 1000 });
+sr.reveal("h3", { duration: 1000 });
+sr.reveal("p", { duration: 1000 });
+sr.reveal(".plans-container", { duration: 1000 });
+sr.reveal(".specialties-container ul li", { duration: 1000 });
+sr.reveal("input", { duration: 1000 });
+sr.reveal("textarea", { duration: 1000 });
